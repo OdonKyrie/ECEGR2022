3c0336ed53bf022fcd22eaa2103df885d3ff0198
OdonKyrie <kfukutomi@seattleu.edu>
2022-04-13 17:01:50 -0700

    Update swap.s

--- Lab_1/swap.s
+++ Lab_1/swap.s
@@ -2,7 +2,7 @@
 
 	.data	# Data declaration section
 
-zing:	.word	0, 1, 2, 3, 4, 5, 6, 7
+zing:	.word	0, 1, 2, 3, 4, 5, 6, 7 ,8
 str1:	.asciz	"zing[4] now has "
 str2:	.asciz	"\nzing[5] now has "
 
